c8fbf024a5f942ed7c9a7a345c192decd383d745
Samuel D. <153951997+samueldolberth@users.noreply.github.com>
2025-08-21 19:24:23 -0300

    Update TryException.py

--- TryException.py
+++ TryException.py
@@ -2,8 +2,8 @@
 Try-except é uma exceção, algo inesperado no código ou apenas algo que não foi tratado diretamente no programa
 Excessões são parte da biblioteca-padrão Python, mas podemos também criar nossas próprias exceções.
 
-o Try permite delimitar o código que pode gerar uma exceção. Ele é completado pela declaraçãp except. Se nenhum erro ocorrer,
-o código dentro de except nem será executado. Dependendo do tipo da exceção, ima mensagem de erro diferente será exibida.
+o Try permite delimitar o código que pode gerar uma exceção. Ele é completado pela declaração except. Se nenhum erro ocorrer,
+o código dentro de except nem será executado. Dependendo do tipo da exceção, uma mensagem de erro diferente será exibida.
 
 No exemplo o FOR não é interrompido, como aconteceria caso não tivéssemos tratado a exceção.
 
@@ -21,9 +21,9 @@ for tentativa in range(3):
     try:
         i = int(input("Digite o índice que quer imprimir: "))
         print(nomes[i])    
-    except ValueError:
+    except ValueError: # Erro no tipo do input
         print("Digite um número!")
-    except IndexError:
+    except IndexError: # Erro no indice da lista
         print("Valor inválido, digite entre -3 e 2")
 '''
 # exemplo 2            testes = (1, abc, 100 )
@@ -46,5 +46,6 @@ for tentativa in range(3):
 
 # Ao usarmos exceções, evitamos retornar e verificar códigos de erro a cada chamada de função, 
 # centralizando o tratamento de erros do bloco em um só lugar e faclitando o tratamento de error
+
 
     
